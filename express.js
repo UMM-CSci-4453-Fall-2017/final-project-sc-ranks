1fd76343482148fa0a7d0c908bdc815c5fd34229
hamme503 <hamme503@morris.umn.edu>
2017-12-12 02:18:24 -0600

    Worked on api handlers, started front-end and have created the back-end tables & procedures for adding/clearing/querying data.

--- express.js
+++ express.js
@@ -2,9 +2,9 @@ async = require("async");
 
 var express=require('express'),
 mysql=require('mysql'),
-    credentials=require('./credentials.json'),
-    app = express(),
-    port = process.env.PORT || 1337;
+credentials=require('./credentials.json'),
+app = express(),
+port = process.env.PORT || 1337;
 
 credentials.host='ids.morris.umn.edu';
 
@@ -13,24 +13,63 @@ var connection = mysql.createConnection(credentials);
 connection.connect(function(err){if(err){console.log(error)}});
 
 app.use(express.static(__dirname + '/src'));
-app.get("/buttons",function(req,res){
+app.get("/searchButtons",function(req,res){
     var sql = 'SELECT * FROM hamme503.search_buttons';
     connection.query(sql,(function(res){return function(err,rows,fields) {
         if(err){console.log("Error obtaining search buttons: ");
-                console.log(err);}
+
+                console.log(err);
+                res.send(err);
+              } else {
+                res.send(rows);
+
+              }
     }})(res));
 
 });
 
-app.get("getProfile",function(req,res){
+app.get("/getProfile",function(req,res){
     var profID = req.param('profID');
     var profName = req.param('profName');
     var sql = 'CALL hamme503.getProfile(' + profID + ", " + profName + ")";
-        connection.query(sql, function(err,row,fields) {
-            if(err){console.log("Error retrieving profile data: ");
-                    console.log(err);}
-        });
-    }(res));
+        connection.query(sql, (function(res){return function(err,rows,fields) {
+            if (err){console.log("Error retrieving profile data: ");
+
+                    console.log(err);
+                    res.send(err);
+                  } else {
+                    res.send(rows);
+                  }
+
+    }})(res));
 
 
 });
+
+app.get("/getLadder",function(req,res){
+  var ladderID = req.param('ladderID');
+  var sql = 'call hamme503.getLadder(' + ladderID + ')';
+    connection.query(sql, (function(res){return function(err,rows,fields) {
+        if(err){console.log("Error retrieving ladder data: ");
+
+                console.log(err);
+                res.send(err);
+              } else {
+                res.send(rows);
+              }
+    }}) (res) );
+} );
+
+app.get("/getGM",function(req,res) {
+  var sql = 'call hamme503.getLadder(263456)';
+  connection.query(sql, (function(res){return function(err,row,fields) {
+      if(err){console.log("Error retrieving ladder data: ");
+              console.log(err);
+              res.send(err);
+            } else {
+              res.send(rows);
+            }
+     }})(res));
+ });
+
+app.listen(port);
--- src/click.js
+++ src/click.js
@@ -4,17 +4,95 @@ angular.module('ranks',[])
     .constant('apiUrl','http://localhost:1337');
 
 function RanksCtrl($scope,RanksApi) {
-
+    $scope.errorMessage='';
     $scope.apiKey="u66e7we2se22mzqxsdezwn2vr3wm8x69";
-    $scope.blizzardApi="https://us.api.battle.net/sc2/"
-    $scope.ladderApi="ladder/:"
-    $scope.profileApi="profile/:"
-    $scope.playerIDs=[];
-    $scope.playerRegions=[];
-    $scope.playerNames=[];
-    $scope.ladderIDs=[];
-    $scope.playerProfile=[];
+    $scope.blizzardAPIURL="https://us.api.battle.net/sc2/ladder";
+    $scope.searchButtons=[];
+    $scope.ladderData=[];
+    $scope.ladderDataRaw=[];
+    $scope.playerProfileData=[];
+    $scope.playerProfileDataRaw=[];
 
+    function getSearchButtons() {
+      $scope.errorMessage='';
+      ranksApi.getButtons()
+        .success(function(data) {
+          $scope.searchButtons = data;
+        })
+        .error(function() {
+          $scope.errorMessage='Error loading search buttons';
+        });
+    }
+
+    function getProfile($event) {
+      $scope.errorMessage='';
+      ranksApi.getProfile($event.target.id, $event.target.)
+        .success(function(data) {
+          $scope.playerProfileData=data;
+        })
+        .error(function() {
+          $scope.errorMessage='Error loading profile';
+        });
+    }
+
+    function getLadder($event){
+      $scope.errorMessage='';
+      ranksApi.getLadder($event.target.id)
+        .success(function(data) {
+          $scope.ladderData = data;
+        })
+        .error(function() {
+          $scope.errorMessage='Error loading ladder';
+        });
+    }
+
+    function getLadderRaw($event){ // Fetch the profile JSON from blizzard
+      $scope.errorMessage ='';
+      ranksApi.getLadderRaw($event.target.id)
+        .success(function(data) {
+          $scope.ladderDataRaw=data;
+        })
+        .error(function() {
+          $scope.errorMessage='Error fetching ladder data from blizzard api';
+        })
+    }
+
+    function getProfileRaw($event){ // Fetch the profile JSON from blizzard
+      $scope.errorMessage='';
+      ranksApi.getProfileRaw($event.target.id)
+      .success(function(data) {
+        $scope.playerProfileDataRaw=data;
+      })
+      .error(function() {
+        $scope.errorMessage='Error fetching profile data from blizzard api';
+      })
+    }
+
+    function ranksApi($http, apiUrl) {
+      return{
+        getButtons(id){
+          var url = apiUrl + "/searchButtons";
+          return $http.get(url);
+        },
+        getProfile(id){
+          var url = apiUrl + "/getProfile?id="+id;
+          return $http.get(url);
+        },
+        getLadder(id){
+          var url = apiUrl + "/getLadder?id="+id;
+          return $http.get(url);
+        },
+        getLadderRaw(id){
+          var url = blizzardAPIURL +
+          "/ladder/"+id+"?locale=en_US&apikey="+apikey;
+          return $http.get(url); // May need to fix this call
+        },
+        getProfileRaw(id, name){
+          var url = blizzardAPIURL +
+          "/profile/"+id+"/"+"/1/"+name+"/?locale=en_US&apikey="+apikey;
+          return $http.get(url); // May ned to fix this call
+        }
+      }
+    }
 
 }
-
