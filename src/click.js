63f0a8c168ba4c7818aba37b1de0c5dd9b7a194f
hamme503 <hamme503@morris.umn.edu>
2017-12-14 03:45:49 -0600

    Update click.js to current version

--- src/click.js
+++ src/click.js
@@ -1,6 +1,6 @@
 angular.module('ranks',[])
     .controller('ranksCtrl',RanksCtrl)
-    .factory('ranksApi',RanksApi)
+    .factory('ranksApi',ranksApi)
     .constant('apiUrl','http://localhost:1337');
 
 function RanksCtrl($scope,RanksApi) {
@@ -13,6 +13,35 @@ function RanksCtrl($scope,RanksApi) {
     $scope.playerProfileData=[];
     $scope.playerProfileDataRaw=[];
 
+    function buttonClick($event) {
+      $scope.errorMessage='';
+      if($event.target.id == 1) {
+        async.series([
+          function(callback) {
+              ranksApi.getProfileRaw(searchProfileId, searchProfileName)
+              .success(function(data) {
+                $scope.playerProfileDataRaw=data;
+              })
+              .error(function () {
+                $scope.errorMessage="Unable to get data for profile: Blizzard API Request Failed"
+              })
+              callback();
+          },
+          function(callback){
+            parseProfileRaw();
+            callback();
+          },
+          function(callback) {
+            ranksApi.insertProfile(playerProfileData);
+            callback()
+          }
+
+        ]
+
+        )
+      }
+    }
+
     function getSearchButtons() {
       $scope.errorMessage='';
       ranksApi.getButtons()
@@ -24,9 +53,9 @@ function RanksCtrl($scope,RanksApi) {
         });
     }
 
-    function getProfile($event) {
+    function getProfile() {
       $scope.errorMessage='';
-      ranksApi.getProfile($event.target.id, $event.target.)
+      ranksApi.getProfile($event.target.id)
         .success(function(data) {
           $scope.playerProfileData=data;
         })
@@ -51,15 +80,16 @@ function RanksCtrl($scope,RanksApi) {
       ranksApi.getLadderRaw($event.target.id)
         .success(function(data) {
           $scope.ladderDataRaw=data;
+          getLadder();
         })
         .error(function() {
           $scope.errorMessage='Error fetching ladder data from blizzard api';
         })
     }
 
-    function getProfileRaw($event){ // Fetch the profile JSON from blizzard
+    function getProfileRaw(){ // Fetch the profile JSON from blizzard
       $scope.errorMessage='';
-      ranksApi.getProfileRaw($event.target.id)
+      ranksApi.getProfileRaw($scope.searchProfileId, $scope.searchProfileName)
       .success(function(data) {
         $scope.playerProfileDataRaw=data;
       })
@@ -67,6 +97,74 @@ function RanksCtrl($scope,RanksApi) {
         $scope.errorMessage='Error fetching profile data from blizzard api';
       })
     }
+    function parseProfileRaw() {
+      console.log("Parsing player profile JSON");
+      var data = [];
+      if ($scope.playerProfileDataRaw.id in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.id);
+      } else {data.push(null);}
+      if ($scope.playerProfileDataRaw.displayName in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.displayName);
+      } else {data.push(null);}
+      if ($scope.playerProfileDataRaw.clanName in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.clanName);
+      } else {data.push(null);}
+      if ($scope.playerProfileDataRaw.clanTag in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.clanTag);
+      } else {data.push(null);}
+      if ($scope.playerProfileDataRaw.career.terranWins in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.career.terranWins);
+      } else {data.push(null);}
+      if ($scope.playerProfileDataRaw.career.protossWins in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.career.protossWins);
+      } else {data.push(null);}
+      if ($scope.playerProfileDataRaw.career.zergWins in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.career.zergWins);
+      } else {data.push(null);}
+      if ($scope.playerProfileDataRaw.career.highest1v1Rank in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.career.highest1v1Rank);
+      } else {data.push(null);}
+
+      if ($scope.playerProfileDataRaw.career.seasonTotalGames in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.career.seasonTotalGames);
+      } else {data.push(null);}
+      if ($scope.playerProfileDataRaw.career.careerTotalGames in $scope.playerProfileDataRaw) {
+        data.push($scope.playerProfileDataRaw.career.careerTotalGames);
+      } else {data.push(null);}
+
+      for (var i = 0; i < $scope.playerProfileDataRaw.season.stats.length; i++) {
+        switch($scope.playerProfileDataRaw.season.stats[i].type) {
+          case "1v1":
+            data[10] = $scope.playerProfileDataRaw.season.stats[i].wins;
+            data[11] = $scope.playerProfileDataRaw.season.stats[i].games;
+            break;
+          case "2v2":
+            data[12] = $scope.playerProfileDataRaw.season.stats[i].wins;
+            data[13] = $scope.playerProfileDataRaw.season.stats[i].games;
+            break;
+          case "3v3":
+            data[14] = $scope.playerProfileDataRaw.season.stats[i].wins;
+            data[15] = $scope.playerProfileDataRaw.season.stats[i].games;
+            break;
+          case "4v4":
+            data[16] = $scope.playerProfileDataRaw.season.stats[i].wins;
+            data[17] = $scope.playerProfileDataRaw.season.stats[i].games;
+            break;
+        }
+      }
+      $scope.playerProfileData=data;
+      console.log("Finished parsing player profile");
+    }
+
+    function parseLadderRaw() {
+      console.log("Parsing ladder JSON");
+      var data = [];
+      for (var i = 0; i<$scope.ladderDataRaw.ladderMembers.length; i++) {
+
+      $scope.ladderData = data;
+    }
+    }
+  }
 
     function ranksApi($http, apiUrl) {
       return{
@@ -84,15 +182,21 @@ function RanksCtrl($scope,RanksApi) {
         },
         getLadderRaw(id){
           var url = blizzardAPIURL +
-          "/ladder/"+id+"?locale=en_US&apikey="+apikey;
+          "/ladder/"+id+"?locale=en_US&apikey="+$scope.apikey;
           return $http.get(url); // May need to fix this call
         },
         getProfileRaw(id, name){
           var url = blizzardAPIURL +
-          "/profile/"+id+"/"+"/1/"+name+"/?locale=en_US&apikey="+apikey;
+          "/profile/"+id+"/1/"+name+"/?locale=en_US&apikey="+$scope.apikey;
           return $http.get(url); // May ned to fix this call
+        },
+        insertProfile(profileData){
+          var url = apiUrl + "/insertProfile?pd="+profileData;
+          return $http.get(url);
+        },
+        insertLadder(ladderData){
+          var url = apiUrl + "/insertLadder?ld="+ladderData;
+          return $http.get(url);
         }
       }
     }
-
-}
